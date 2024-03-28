35e8032f80406b1eaac0fe392d961fa41333b17e
akshiyal <aartibenkshiyal@gmail.com>
2024-03-28 09:46:29 +0530

    add

---
+++ 08-mix/0801-print-biodata-mix.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = 'Alexandra Abramov';
+  String dob = 'July 14, 1975';
+  String mobile = '99-9999999999';
+
+  print('Name   : $name');
+  print('DOB    : $dob');
+  print('Mobile : $mobile');
+}
