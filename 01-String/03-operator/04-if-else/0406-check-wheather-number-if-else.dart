cd3011302424af04aa17276e591064397d93fd09
akshiyal <aartibenkshiyal@gmail.com>
2024-03-28 09:30:47 +0530

    add

---
+++ 01-String/03-operator/04-if-else/0406-check-wheather-number-if-else.dart
@@ -0,0 +1,10 @@
+void main() {
+  var n1 = 23;
+  if (n1 > 0) {
+    print("number is positive");
+  } else if (n1 < 0) {
+    print("number is negative");
+  } else {
+    print("number is zero");
+  }
+}
