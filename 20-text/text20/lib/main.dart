9d27c3fa31bd824be31557eb56ae95ba682d1dee
akshiyal <aartibenkshiyal@gmail.com>
2024-03-30 10:54:21 +0530

    add

---
+++ 20-text/text20/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+//import 'package:text20/text2001.dart';
+//import 'package:text20/text2002.dart';
+//import 'package:text20/text2002.dart';
+//import 'package:text20/text2003.dart';
+import 'package:text20/text2004.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: text4(),
+    );
+  }
+}
