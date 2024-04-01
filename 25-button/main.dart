a7e0c8c370c05e22f3410f1272ca02b6f65d5d41
akshiyal <aartibenkshiyal@gmail.com>
2024-04-01 10:29:40 +0530

    add

---
+++ 25-button/main.dart
@@ -0,0 +1,19 @@
+import 'package:button25/btn2502.dart';
+import 'package:flutter/material.dart';
+
+//import 'package:button25/btn2501.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: btn2(),
+    );
+  }
+}
