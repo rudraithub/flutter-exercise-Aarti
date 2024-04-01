6058e392eafc098b188412a7b4da305173d47c0a
akshiyal <aartibenkshiyal@gmail.com>
2024-04-01 09:08:52 +0530

    add

---
+++ 21-textfield/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//import 'package:textfield21/textf2101.dart';
+import 'package:textfield21/textf2102.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: text2(),
+    );
+  }
+}
