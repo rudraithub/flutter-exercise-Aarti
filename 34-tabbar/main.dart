5cf0f1adf28dab6850ebbf1c4dd2d3b8e67c4286
akshiyal <aartibenkshiyal@gmail.com>
2024-04-02 10:08:39 +0530

    add

---
+++ 34-tabbar/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:tabbar34/tab3401.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: icon1(),
+    );
+  }
+}
