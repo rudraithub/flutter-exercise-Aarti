49a960051404d55fc468526b88036300347c0eb7
akshiyal <aartibenkshiyal@gmail.com>
2024-04-02 09:01:09 +0530

    add

---
+++ 27-listview/list2701.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class list1 extends StatelessWidget {
+  const list1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+            "ListView",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          leading: Icon(
+            Icons.home,
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.brown),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 0'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 1'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 1'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 2'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 3'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 4'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 5'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 6'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 7'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 8'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 9'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 10'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 11'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 12'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 13'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 14'),
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            trailing: Text('Rudra It Hub'),
+            title: Text('List View 15'),
+          ),
+        ],
+      ),
+    );
+  }
+}
