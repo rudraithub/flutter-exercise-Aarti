20d56ebe65cde97763fbe4cf986f4547581ac053
akshiyal <aartibenkshiyal@gmail.com>
2024-04-02 09:50:51 +0530

    add

---
+++ 31-checkbox/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//import 'package:checkbox31/check3101.dart';
+import 'package:checkbox31/check3102.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: check2(),
+    );
+  }
+}
