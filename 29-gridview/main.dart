27a76712afdf4401a8b2f0755e20c25fcc251c07
akshiyal <aartibenkshiyal@gmail.com>
2024-04-02 09:31:54 +0530

    add

---
+++ 29-gridview/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//import 'package:gridview29/grid2901.dart';
+import 'package:gridview29/grid2902.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: tex19(),
+    );
+  }
+}
