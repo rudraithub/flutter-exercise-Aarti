f286480fc3ecdbc8497117eb8c4e244d5b92db8e
akshiyal <aartibenkshiyal@gmail.com>
2024-04-02 10:13:03 +0530

    add

---
+++ 35-pagecontroller/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:pagecontroller35/page3501.dart';
+
+//import 'package:pagecontroller35/page3501.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: icon1(),
+    );
+  }
+}
