3d1df2a837047c48be9e6ba7a319713cbaf10cf8
akshiyal <aartibenkshiyal@gmail.com>
2024-04-04 10:48:01 +0530

    add

---
+++ 46-simpledialog/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:simpledialod46/simple4601.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: d1(),
+    );
+  }
+}
